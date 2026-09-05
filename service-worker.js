26d6737a600971933c427e49201eb234675a0824
gdspjn2myr <gdsp.jn2myr@gmail.com>
2026-09-05 10:52:54 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // DAN supaya user dapat notif "Versi baru tersedia" di aplikasi.
 // ============================================================================
 
-const APP_VERSION = '1.3.0';
+const APP_VERSION = '1.3.1';
 const CACHE_NAME = 'wsmart-shell-v' + APP_VERSION;
 const APP_SHELL = [
   './',
